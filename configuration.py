a17fe9f3fbbe0fd5dad95832f18e997b07baaafd
creeston <mityy2012@gmail.com>
2020-05-30 22:15:29 +0300

    Return None if environment variable not exist

--- configuration.py
+++ configuration.py
@@ -1,35 +1,39 @@
 import os
 
 
+def try_get_env(key):
+    if key in os.environ:
+        return os.environ[key]
+    return None
+
+
 tg_credentials = {
-    "api_id": int(os.environ["TG_API_ID"]),
-    "api_hash": os.environ["TG_API_HASH"]
+    "api_id": try_get_env("TG_API_ID"),
+    "api_hash": try_get_env("TG_API_HASH")
 }
 
 vk_credentials = {
-    "phone_num": os.environ["VK_PHONE_NUM"],
-    "password": os.environ["VK_PASS"]
+    "phone_num": try_get_env("VK_PHONE_NUM"),
+    "password": try_get_env("VK_PASS")
 }
 
 
 cloudinary_config = {
-  "cloud_name": os.environ["CLOUD_NAME"],
-  "api_key": os.environ["CLOUD_API_KEY"],
-  "api_secret": os.environ["CLOUD_API_SECRET"]
+  "cloud_name": try_get_env("CLOUD_NAME"),
+  "api_key": try_get_env("CLOUD_API_KEY"),
+  "api_secret": try_get_env("CLOUD_API_SECRET")
 }
 
 sql_server = {
-    "server":  os.environ["SQL_SERVER"],
-    "database": os.environ["SQL_DATABASE"],
-    "username": os.environ["SQL_USERNAME"],
-    "password": os.environ["SQL_PASS"]
+    "server":  try_get_env("SQL_SERVER"),
+    "database": try_get_env("SQL_DATABASE"),
+    "username": try_get_env("SQL_USERNAME"),
+    "password": try_get_env("SQL_PASS")
 }
-
-print(sql_server)
 
 oauth = {
     "facebook": {
-        "id": os.environ["OAUTH_FB_ID"],
-        "secret": os.environ["OAUTH_FB_SECRET"]
+        "id": try_get_env("OAUTH_FB_ID"),
+        "secret": try_get_env("OAUTH_FB_SECRET")
     }
 }
