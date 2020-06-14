7f780413ce084ff345b8b4e93f0df614b574fcbe
creeston <mityy2012@gmail.com>
2020-06-14 13:54:33 +0300

    Download model to temp directory

--- TrainClassifier/__init__.py
+++ TrainClassifier/__init__.py
@@ -5,6 +5,8 @@ from azure.storage.blob import ContainerClient
 from azure.cosmosdb.table.tableservice import TableService
 from trainers import labels, ClassifierTrainer
 from common import Logger
+from tempfile import NamedTemporaryFile, mkdtemp
+
 
 connection_string = os.environ['AzureWebJobsStorage']
 service = TableService(connection_string=connection_string, is_emulated=True)
@@ -26,23 +28,23 @@ def load_data():
     return raw_data
 
 
-def download_model() -> str:
-    itos_path = "temp\\ULMFit\\itos.pkl"
-    weight_path = "temp\\ULMFit\\lm_5_ep_lr2-3_5_stlr.pth"
-    enc_weight_path = "temp\\ULMFit\\lm_5_ep_lr2-3_5_stlr_enc.pth"
+def download_model(tempdir):
+    itos_path = os.path.join(tempdir, "itos.pkl")
+    weight_path = os.path.join(tempdir, "lm_5_ep_lr2-3_5_stlr.pth")
+    enc_weight_path = os.path.join(tempdir, "lm_5_ep_lr2-3_5_stlr_enc.pth")
 
-    model_dir = "temp\\ULMFit"
-    if os.path.exists(model_dir):
-        if os.path.exists(itos_path):
-            os.remove(itos_path)
-        if os.path.exists(weight_path):
-            os.remove(weight_path)
-        if os.path.exists(enc_weight_path):
-            os.remove(enc_weight_path)
-    else:
-        if not os.path.exists("temp"):
-            os.mkdir("temp")
-        os.mkdir(model_dir)
+    #model_dir = "temp\\ULMFit"
+    #if os.path.exists(model_dir):
+    #    if os.path.exists(itos_path):
+    #        os.remove(itos_path)
+    #    if os.path.exists(weight_path):
+    ##        os.remove(weight_path)
+     #   if os.path.exists(enc_weight_path):
+     #       os.remove(enc_weight_path)
+    ##else:
+     #   if not os.path.exists("temp"):
+     #       os.mkdir("temp")
+     #   os.mkdir(model_dir)
 
     with open(itos_path, "wb") as f:
         model_service.download_blob(itos_blob).readinto(f)
@@ -50,7 +52,7 @@ def download_model() -> str:
         model_service.download_blob(weights_blob).readinto(f)
     with open(enc_weight_path, "wb") as f:
         model_service.download_blob(enc_weights_blob).readinto(f)
-    return model_dir
+    return itos_path, weight_path, enc_weight_path
 
 
 def main(msg) -> None:
@@ -58,11 +60,22 @@ def main(msg) -> None:
     logging.info("Start loading data for training")
     data = load_data()
     logging.info("Start downloading base model")
-    model_path = download_model()
-    weights_pretrained = os.path.abspath(model_path + '\\lm_5_ep_lr2-3_5_stlr')
-    itos_pretrained = os.path.abspath(model_path + '\\itos')
-    pretrained_data = (weights_pretrained, itos_pretrained)
-    logging.info("Start training")
-    train_model = os.path.abspath("trained_model")
-    result = trainer.train_classifier_model(data, pretrained_data, train_model)
-    logging.info("Training finished " + str(result))
+    tempdir = mkdtemp()
+    itos_file, weight_file, enc_weight_file = download_model(tempdir)
+    try:
+        weights_pretrained = os.path.join(tempdir, 'lm_5_ep_lr2-3_5_stlr')
+        itos_pretrained = os.path.join(tempdir, 'itos')
+        pretrained_data = (weights_pretrained, itos_pretrained)
+        logging.info("Start training")
+        train_model = os.path.abspath("trained_model")
+        result = trainer.train_classifier_model(data, pretrained_data, train_model)
+        logging.info("Training finished " + str(result))
+    finally:
+        os.remove(itos_file)
+        os.remove(weight_file)
+        os.remove(enc_weight_file)
+        os.rmdir(tempdir)
+
+
+if __name__ == "__main__":
+    main(None)
