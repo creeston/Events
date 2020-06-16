ccc655dcdab23714225d4dacea417bde6b9036aa
creeston <mityy2012@gmail.com>
2020-06-16 11:53:56 +0300

    Add model blob input binding

--- TrainClassifier/__init__.py
+++ TrainClassifier/__init__.py
@@ -5,16 +5,13 @@ from azure.storage.blob import ContainerClient
 from azure.cosmosdb.table.tableservice import TableService
 from trainers import labels, ClassifierTrainer
 from common import Logger
-from tempfile import NamedTemporaryFile, mkdtemp
+from tempfile import mkdtemp
 
 
 connection_string = os.environ['AzureWebJobsStorage']
 service = TableService(connection_string=connection_string, is_emulated=True)
 
 table = "eventClassificationTrainingData"
-weights_blob = "ULMFit/lm_5_ep_lr2-3_5_stlr.pth"
-enc_weights_blob = "ULMFit/lm_5_ep_lr2-3_5_stlr_enc.pth"
-itos_blob = "ULMFit/itos.pkl"
 model_service = ContainerClient.from_connection_string(connection_string, "model")
 
 
@@ -28,41 +25,31 @@ def load_data():
     return raw_data
 
 
-def download_model(tempdir):
+def download_model(tempdir,
+                   itos_blob: func.InputStream,
+                   weight_blob: func.InputStream,
+                   enc_weight_blob: func.InputStream):
     itos_path = os.path.join(tempdir, "itos.pkl")
     weight_path = os.path.join(tempdir, "lm_5_ep_lr2-3_5_stlr.pth")
     enc_weight_path = os.path.join(tempdir, "lm_5_ep_lr2-3_5_stlr_enc.pth")
 
-    #model_dir = "temp\\ULMFit"
-    #if os.path.exists(model_dir):
-    #    if os.path.exists(itos_path):
-    #        os.remove(itos_path)
-    #    if os.path.exists(weight_path):
-    ##        os.remove(weight_path)
-     #   if os.path.exists(enc_weight_path):
-     #       os.remove(enc_weight_path)
-    ##else:
-     #   if not os.path.exists("temp"):
-     #       os.mkdir("temp")
-     #   os.mkdir(model_dir)
-
     with open(itos_path, "wb") as f:
-        model_service.download_blob(itos_blob).readinto(f)
+        f.write(itos_blob.read())
     with open(weight_path, "wb") as f:
-        model_service.download_blob(weights_blob).readinto(f)
+        f.write(weight_blob.read())
     with open(enc_weight_path, "wb") as f:
-        model_service.download_blob(enc_weights_blob).readinto(f)
+        f.write(enc_weight_blob.read())
     return itos_path, weight_path, enc_weight_path
 
 
-def main(msg) -> None:
+def main(msg, itos_blob: func.InputStream, enc_weight_blob: func.InputStream, weight_blob: func.InputStream) -> None:
     trainer = ClassifierTrainer(Logger())
     logging.info("Start loading data for training")
     data = load_data()
     logging.info("Start downloading base model")
     tempdir = mkdtemp()
     export_path = None
-    itos_file, weight_file, enc_weight_file = download_model(tempdir)
+    itos_file, weight_file, enc_weight_file = download_model(tempdir, itos_blob, weight_blob, enc_weight_blob)
     try:
         weights_pretrained = os.path.join(tempdir, 'lm_5_ep_lr2-3_5_stlr')
         itos_pretrained = os.path.join(tempdir, 'itos')
@@ -80,4 +67,4 @@ def main(msg) -> None:
 
 
 if __name__ == "__main__":
-    main(None)
+    main(None, None, None, None)
