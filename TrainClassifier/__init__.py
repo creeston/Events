a471d1e08b56813056e91a0f2c5be29438a8b5f8
creeston <mityy2012@gmail.com>
2020-06-16 14:35:28 +0300

    Fix bindings

--- TrainClassifier/__init__.py
+++ TrainClassifier/__init__.py
@@ -15,9 +15,9 @@ table = "eventClassificationTrainingData"
 model_service = ContainerClient.from_connection_string(connection_string, "model")
 
 
-def load_data():
+def load_data(training_data):
     raw_data = []
-    for entity in service.query_entities(table):
+    for entity in training_data:# service.query_entities(table):
         text = entity['Text']
         event_labels = entity['Labels'].split(',')
         label_ids = [labels.index(label) for label in event_labels if label in labels]
@@ -42,14 +42,16 @@ def download_model(tempdir,
     return itos_path, weight_path, enc_weight_path
 
 
-def main(msg, itos_blob: func.InputStream, enc_weight_blob: func.InputStream, weight_blob: func.InputStream) -> None:
+def main(msg,
+         itos: func.InputStream, encweight: func.InputStream, weight: func.InputStream,
+         data) -> None:
     trainer = ClassifierTrainer(Logger())
     logging.info("Start loading data for training")
-    data = load_data()
+    data = load_data(data)
     logging.info("Start downloading base model")
     tempdir = mkdtemp()
     export_path = None
-    itos_file, weight_file, enc_weight_file = download_model(tempdir, itos_blob, weight_blob, enc_weight_blob)
+    itos_file, weight_file, enc_weight_file = download_model(tempdir, itos, weight, encweight)
     try:
         weights_pretrained = os.path.join(tempdir, 'lm_5_ep_lr2-3_5_stlr')
         itos_pretrained = os.path.join(tempdir, 'itos')
